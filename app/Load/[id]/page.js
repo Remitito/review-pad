99843146c382166f1a75b7e15e2246aed0104b80
Remitito <jrossot@protonmail.com>
2023-11-02 17:12:44 +0000

    Initial data fetching on notepad component loading added

--- app/Load/[id]/page.js
+++ app/Load/[id]/page.js
@@ -1,22 +1,24 @@
-"use client";
-
-const Load = ({ params }) => {
-  const padId = params.id;
-
-  const loadNotepad = async () => {
-    console.log(padId);
+const loadNotepad = async (padId) => {
+  if (padId) {
     try {
-      const response = await fetch(`/api/notepad/${padId}`);
-      const data = await response.json();
-      console.log(data);
+      const response = await fetch(
+        `http://localhost:3000/api/notepad/${padId}`
+      );
+      return response.json();
     } catch (error) {
       console.error("There was an error:", error);
     }
-  };
+  }
+};
+
+const Load = async ({ params }) => {
+  const padId = params.id;
+  const padData = await loadNotepad(padId);
+  const notepad = padData.notepad;
 
   return (
     <div>
-      <button onClick={() => loadNotepad()}>Load {padId}</button>
+      <h1>{notepad.name}</h1>
     </div>
   );
 };
